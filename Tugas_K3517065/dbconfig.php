8d9e435c4fc4ca0266a3b92e414ec1539894ec9d
joseK3517065 <49801406+joseK3517065@users.noreply.github.com>
2019-04-20 11:07:10 +0700

    Add files via upload

---
+++ Tugas_K3517065/dbconfig.php
@@ -0,0 +1,3 @@
+<?php
+	$db = mysqli_connect("localhost", "root", "aliun", "crazy") or die(mysqli_error());
+?>
---
+++ Tugas_K3517065/logout.php
@@ -0,0 +1,5 @@
+<?php
+	setcookie('username', '' , time()-3600*24*7,"/tugas_K3517065");
+	header("Location: index.php");
+
+?>
